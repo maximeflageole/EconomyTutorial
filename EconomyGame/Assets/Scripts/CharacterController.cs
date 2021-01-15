3ed5dc95a3567b6aeab2aa0ec533729abd9111a7
Nixonm2 <maximeflageole@gmail.com>
2021-01-15 16:33:17 -0500

    Project setup [Tilemaps: Creating our world] - Creation of the project - Tile palette - Creation of 3 different tilemaps: water, ground and decorations [Character, Camera, Controls] - Creating a character inputs and movements script - Adding colliders on the character and on the water tilemap - Creating a camera following the character using lerps

---
+++ EconomyGame/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float m_acceleration = 50.0f;
+    [SerializeField] private float m_maxSpeed = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector2.right;
+        }
+
+        if (movement.magnitude > 0f)
+        {
+            movement.Normalize();
+        }
+        
+        var rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d.AddForce(movement * m_acceleration);
+
+        if (rigidbody2d.velocity.magnitude >= m_maxSpeed)
+        {
+            rigidbody2d.velocity = rigidbody2d.velocity.normalized * m_maxSpeed;
+        }
+    }
+}
---
+++ EconomyGame/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] private Transform m_target;
+    [SerializeField] private float m_lerpSpeed = 0.1f;
+    private Vector3 m_offset;
+
+    void Awake()
+    {
+        m_offset = transform.position;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = m_target.position;
+        Vector3 lerpedPosition = Vector3.Lerp(transform.position, targetPosition + m_offset, m_lerpSpeed);
+        transform.position = lerpedPosition;
+    }
+}
