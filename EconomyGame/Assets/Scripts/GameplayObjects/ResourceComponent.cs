fdbdf032c33f395978cb2b3bfd755a35b231a8c6
Nixonm2 <maximeflageole@gmail.com>
2021-01-21 15:58:19 -0500

    Small project changes in the organisation. Not much done

---
+++ EconomyGame/Assets/Scripts/GameplayObjects/ResourceComponent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ResourceComponent : Selectable
+{
+    [SerializeField] private SpriteRenderer m_renderer;
+    [SerializeField] private ResourceData m_data;
+    private EResource m_resourceType;
+    private int m_resourceAmount;
+    private int m_currentHealth;
+    private int m_maxHealth;
+
+    private void Awake()
+    {
+        m_resourceType = m_data.ResourceType;
+        m_resourceAmount = m_data.ResourceAmount;
+        m_currentHealth = m_data.Health;
+        m_maxHealth = m_data.Health;
+        m_renderer.sprite = m_data.Sprite;
+    }
+
+    public override void Damage(int amount)
+    {
+        m_currentHealth -= amount;
+        m_currentHealth = Mathf.Max(m_currentHealth, 0);
+        if (m_currentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    public override void Die()
+    {
+        CharacterController.Instance.GetResources(m_resourceType, m_resourceAmount);
+        base.Die();
+    }
+}
