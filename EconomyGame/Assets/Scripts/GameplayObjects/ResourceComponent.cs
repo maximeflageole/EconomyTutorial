d87b2e076660b4baa97ab367ba2bbe320a1175e6
Nixonm2 <maximeflageole@gmail.com>
2021-02-05 17:36:18 -0500

    [Code][Gameplay] Adding a glow and flicker component, which controls a sprite color and size over time Adding an empty building component which inherits Selectable

--- EconomyGame/Assets/Scripts/GameplayObjects/ResourceComponent.cs
+++ EconomyGame/Assets/Scripts/GameplayObjects/ResourceComponent.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ResourceComponent : Selectable
 {
+    [SerializeField] private List<AudioClip> m_onHitClips;
+    [SerializeField] private AudioSource m_audioOnHit;
     [SerializeField] private ParticleSystem m_particlesOnHit;
     [SerializeField] private EResource m_resourceType;
     [SerializeField] private int m_resourceAmount;
@@ -15,6 +18,8 @@ public class ResourceComponent : Selectable
 
     public override void Damage(int amount)
     {
+        m_audioOnHit.clip = m_onHitClips[Random.Range(0, m_onHitClips.Count - 1)];
+        m_audioOnHit.Play();
         m_particlesOnHit.Play();
         m_currentHealth -= amount;
         m_currentHealth = Mathf.Max(m_currentHealth, 0);
---
+++ EconomyGame/Assets/Scripts/Tools and universal components/GlowAndFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class GlowAndFlicker : MonoBehaviour
+{
+    [SerializeField] private Gradient m_colors;
+    [SerializeField] private AnimationCurve m_sizeOverTime;
+    [SerializeField] private float m_loopTime;
+    private SpriteRenderer m_spriteRenderer;
+    private float m_currentTime = 0;
+
+    private void Start()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        m_currentTime %= m_loopTime;
+        m_currentTime += Time.deltaTime;
+        float timePercentage = m_currentTime / m_loopTime;
+        m_spriteRenderer.color = m_colors.Evaluate(timePercentage);
+        float currentSize = m_sizeOverTime.Evaluate(timePercentage);
+        transform.localScale = new Vector3(currentSize, currentSize, currentSize);
+    }
+}
